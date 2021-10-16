9513f938438cb0047b7454b7cb90e7e06753d405
iborymagic <mlch8023@gmail.com>
2021-10-16 13:39:52 +0900

    feat: render 함수 구조 변경 - 시작 함수와 재귀 함수를 분리 - documentFragment를 이용하도록 수정

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import Counter from './Counter.js';
 import { render } from './lib/PeactDOM.js';
 
-const rootElement = document.getElementById('root');
-
-render(Counter(), rootElement);
+render(Counter, document.getElementById('root'));
