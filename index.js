6eaf8780d22643b8e8ed71c5b0597ed65c23ef3c
365kim <365listener@gmail.com>
2021-10-18 00:33:00 +0900

    refactor: Model에 변경이 발생한 경우에만 View 업데이트

--- index.js
+++ index.js
@@ -3,33 +3,41 @@ import { parse } from './parser.js';
 import { counterTemplate } from './template.js';
 
 let count = 0;
-const UPDATE_TIME_INTERVAL = 150;
-const getVirtualDOM = () => parse(counterTemplate(count));
+
 const $root = document.querySelector('#root');
+
+const getVirtualDOM = () => {
+  const template = counterTemplate(count);
+
+  return parse(template);
+};
+
+const updateDOM = () => {
+  const virtualDOM = getVirtualDOM();
+
+  render(virtualDOM, $root);
+};
+
 const handleClickCounter = (e) => {
-  const isSubtractButtonClicked = !!e.target.closest('.btn-subtract');
   const isAddButtonClicked = !!e.target.closest('.btn-add');
+  const isSubtractButtonClicked = !!e.target.closest('.btn-subtract');
   const isResetButtonClicked = !!e.target.closest('.btn-reset');
 
-  if (isSubtractButtonClicked) {
+  if (isAddButtonClicked) {
     count += 1;
+    updateDOM();
     return;
   }
-  if (isAddButtonClicked) {
+  if (isSubtractButtonClicked) {
     count -= 1;
+    updateDOM();
     return;
   }
   if (isResetButtonClicked) {
     count = 0;
+    updateDOM();
   }
 };
 
-const virtualDOM = getVirtualDOM();
+updateDOM();
 $root.addEventListener('click', handleClickCounter);
-render(virtualDOM, $root);
-
-setInterval(() => {
-  const virtualDOM = getVirtualDOM();
-
-  render(virtualDOM, $root);
-}, UPDATE_TIME_INTERVAL);
