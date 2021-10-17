83d86bd20a3a4852aaf1551fe57278b48037ca69
iborymagic <mlch8023@gmail.com>
2021-10-17 21:01:34 +0900

    feat: state와 useState 구현

--- Counter.js
+++ Counter.js
@@ -1,26 +1,41 @@
 import { createElement } from './lib/Peact.js';
+import useState from './lib/useState.js';
 
 function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
   return createElement(
     'div',
     { class: 'container' },
-    createElement('span', { class: 'count' }, createElement('text', {}, '0')),
+    createElement('span', { class: 'count' }, createElement('text', {}, count)),
     createElement(
       'div',
       { class: 'btn-group' },
       createElement(
         'button',
-        {},
+        { click: decrease },
         createElement('strong', {}, createElement('text', {}, '-'))
       ),
       createElement(
         'button',
-        {},
+        { click: reset },
         createElement('strong', {}, createElement('text', {}, 'RESET'))
       ),
       createElement(
         'button',
-        {},
+        { click: increase },
         createElement('strong', {}, createElement('text', {}, '+'))
       )
     )
