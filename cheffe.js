af31632c9e1621a3c9f518e1a4ccf72010d194fb
Shim MunSeong <Puterism.K@gmail.com>
2021-10-18 23:56:23 +0900

    refactor: 사용하지 않는 key 속성  추가 로직 삭제

--- cheffe.js
+++ cheffe.js
@@ -1,26 +1,18 @@
 export class VNode {
-  constructor({ key, nodeName, attributes, children }) {
-    this.key = key;
+  constructor({ nodeName, attributes, children }) {
     this.nodeName = nodeName;
     this.attributes = attributes;
     this.children = children;
   }
 }
 
-const createElement = (() => {
-  let nextId = -1;
-
-  return (nodeName, attributes, children) => {
-    nextId += 1;
-
-    return new VNode({
-      key: nextId,
-      nodeName,
-      attributes,
-      children,
-    });
-  };
-})();
+const createElement = (nodeName, attributes, children) => {
+  return new VNode({
+    nodeName,
+    attributes,
+    children,
+  });
+};
 
 const Cheffe = {
   createElement,
