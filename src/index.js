1aa165eed222dec58f179261cf9155fd3d1951d4
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-10-19 23:46:33 +0900

    feat: VDOM 업데이트 후 렌더링하는 updateDOM 함수 구현

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import createVDOM from "./lib/createVDOM.js";
 import render from "./lib/render.js";
 
 let count = 0;
+const rootElement = document.querySelector("#root");
 
 const Element = () => {
   return createVDOM(
@@ -20,15 +21,24 @@ const Element = () => {
 
 function onDecreaseNumber() {
   count--;
+  updateDOM(Element(), rootElement);
 }
 
 function onResetButton() {
   count = 0;
+  updateDOM(Element(), rootElement);
 }
 
 function onIncreaseNumber() {
   count++;
+  updateDOM(Element(), rootElement);
 }
 
-const rootElement = document.querySelector("#root");
+// 초기 rendering
 render(Element(), rootElement);
+
+function updateDOM(VDOM, rootElement) {
+  // 전체 리렌더링을 하고 있어서, 초기화 해주는 작업이 필요해서 추가한 코드
+  rootElement.innerHTML = "";
+  render(VDOM, rootElement);
+}
