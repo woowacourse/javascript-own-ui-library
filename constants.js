ffc64c454806db3fee38083a4b12c4858a19257f
Shim MunSeong <puterism.k@gmail.com>
2021-10-20 13:14:46 +0900

    chore: 메시지 오타 수정
    Co-authored-by: 365kim <60066472+365kim@users.noreply.github.com>

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ export const ERROR_MESSAGE = {
   RENDER: {
     EMPTY_ROOT_ELEMENT:
       'rootElement가 비어있습니다. 렌더할 HTML 요소를 parameter에 할당해주세요.',
-    ONLY_CALL_ONCE: 'render는 한 번만 호츨할 수 있습니다.',
+    ONLY_CALL_ONCE: 'render는 한 번만 호출할 수 있습니다.',
     INVALID_VNODE:
       '`Cheffe.createElement`로 생성한 객체를 parameter에 할당해주세요.',
   },
