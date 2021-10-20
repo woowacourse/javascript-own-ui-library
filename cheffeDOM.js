6ef15dc82412e31dff17f6b08e5e23a75a8cef5d
Shim MunSeong <Puterism.K@gmail.com>
2021-10-20 22:59:20 +0900

    refactor: 코드 리뷰 반영
    - `isBindEvent` -> `isEventHandler`로 네이밍 변경
    
    - VNode를 판별하는 조건문을 더 명확하게 수정
    
    - 불필요한 주석 삭제

--- cheffeDOM.js
+++ cheffeDOM.js
@@ -25,10 +25,10 @@ function createRealDOMNode(vNode) {
   if (attributes) {
     // 모든 속성을 순회한다.
     Object.entries(attributes).forEach(([key, value]) => {
-      const isBindEvent = key.slice(0, 2) === 'on';
+      const isEventHandler = key.slice(0, 2) === 'on';
 
       // 속성이 on으로 시작하고, value가 함수이면, 이벤트를 bind한다.
-      if (isBindEvent && typeof value === 'function') {
+      if (isEventHandler && typeof value === 'function') {
         const eventType = key.slice(2);
 
         bindEvent(node, eventType, value);
@@ -52,8 +52,8 @@ function createRealDOMNode(vNode) {
 function updateRealDOMNode(vNode, targetNode) {
   const fragment = document.createDocumentFragment();
   const node = createRealDOMNode(vNode);
-  fragment.appendChild(node);
 
+  fragment.appendChild(node);
   targetNode.parentNode.replaceChild(fragment, targetNode);
   console.log('DOM 업데이트됨');
 }
@@ -62,7 +62,6 @@ function updateRealDOMNode(vNode, targetNode) {
 // 루트 노드부터 시작해 상위 노드를 순서대로 쭉 비교한다.
 // 상위 노드가 변경되었으면, 하위 노드들도 변경된 상태로 간주한다.
 function diff(node, vNode) {
-  // node가 텍스트 노드일 때, vNode가 VNode가 아닌 다른 타입이라면
   if (node instanceof Text && !(vNode instanceof VNode)) {
     // node 문자열과 vNode 문자열이 같지 않다면 텍스트 노드가 다르다고 판단한다.
     if (node.data !== `${vNode}`) {
@@ -122,7 +121,7 @@ function diff(node, vNode) {
 }
 
 function update(vNode) {
-  if (typeof vNode !== 'object') {
+  if (!(vNode instanceof VNode)) {
     throw new Error(ERROR_MESSAGE.RENDER.INVALID_VNODE);
   }
 
@@ -138,7 +137,7 @@ function render(vElement, rootElement) {
     throw new Error(ERROR_MESSAGE.RENDER.ONLY_CALL_ONCE);
   }
 
-  if (typeof vNode !== 'object') {
+  if (!(vNode instanceof VNode)) {
     throw new Error(ERROR_MESSAGE.RENDER.INVALID_VNODE);
   }
 
