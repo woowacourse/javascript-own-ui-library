16c30cf5cb952069f3dd2b5a5920a3444b0d9ea1
yujo <yujo.dev@gmail.com>
2021-10-25 01:32:52 +0900

    feat: setNodeObject function

--- src/utils/render.ts
+++ src/utils/render.ts
@@ -1,19 +1,38 @@
+import isEmptyObject from "./isEmptyObject";
+
 const render = (() => {
-  let oldNode;
+  const oldNode = {};
 
-  const inner = (target: Element, node: Node): void => {
-    if (!oldNode) {
-      target.appendChild(node);
+  const setNodeObject = (node: Element, obj = oldNode) => {
+    if (node.hasChildNodes()) {
+      obj[node.getAttribute("key")] = node;
 
-      oldNode = node.childNodes;
+      [...node.children].forEach((child) => {
+        setNodeObject(child, obj[node.getAttribute("key")]);
+      });
     } else {
-      // NOTE 비교 없이 덮어쓰는 경우
-      target.innerHTML = "";
-      target.appendChild(node);
+      obj[node.getAttribute("key")] = node;
     }
   };
 
-  return inner;
+  const render = (target: Element, node: Element): void => {
+    if (isEmptyObject(oldNode)) {
+      target.appendChild(node);
+
+      setNodeObject(node);
+    } else {
+      // NOTE 비교 없이 덮어쓰는 경우
+      // target.innerHTML = "";
+      // target.appendChild(node);
+
+      const newNode = {};
+      setNodeObject(node, newNode);
+
+      console.log(newNode);
+    }
+  };
+
+  return render;
 })();
 
 export default render;
