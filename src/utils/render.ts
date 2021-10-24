6d5514b699cbc594d55aeaeea5ec83421e30ba6c
yujo <yujo.dev@gmail.com>
2021-10-25 03:38:55 +0900

    feat: 노드를 순회하며 비교하여 변경된 노드만 바꾸도록 수정

--- src/utils/render.ts
+++ src/utils/render.ts
@@ -1,38 +1,86 @@
+import isChanged from "./isChanged";
 import isEmptyObject from "./isEmptyObject";
 
 const render = (() => {
-  const oldNode = {};
+  const oldNodeObj = {};
 
-  const setNodeObject = (node: Element, obj = oldNode) => {
+  const getNodeObject = (node: Element, obj = {}) => {
     if (node.hasChildNodes()) {
       obj[node.getAttribute("key")] = node;
 
       [...node.children].forEach((child) => {
-        setNodeObject(child, obj[node.getAttribute("key")]);
+        getNodeObject(child, obj[node.getAttribute("key")]);
       });
     } else {
       obj[node.getAttribute("key")] = node;
     }
+
+    return obj;
   };
 
-  const render = (target: Element, node: Element): void => {
-    if (isEmptyObject(oldNode)) {
-      target.appendChild(node);
+  const rerender = (
+    target: Element,
+    newNodeObj: { [key: string]: Element },
+    oldNodeObj: { [key: string]: Element }
+  ) => {
+    if (isEmptyObject(newNodeObj) || isEmptyObject(oldNodeObj)) return;
 
-      setNodeObject(node);
-    } else {
-      // NOTE 비교 없이 덮어쓰는 경우
-      // target.innerHTML = "";
-      // target.appendChild(node);
+    for (const key of Object.keys(newNodeObj)) {
+      if (!isChanged(newNodeObj[key], oldNodeObj[key])) {
+        continue;
+      }
 
-      const newNode = {};
-      setNodeObject(node, newNode);
+      if (isChanged(newNodeObj[key], oldNodeObj[key])) {
+        target.querySelector(`[key="${key}"]`).replaceWith(newNodeObj[key]);
+        continue;
+      }
 
-      console.log(newNode);
+      // 새로운 노드
+      if (!oldNodeObj.hasOwnProperty(key)) {
+        target.appendChild(newNodeObj[key]);
+        oldNodeObj[key] = newNodeObj[key];
+
+        continue;
+      }
+
+      // new 자식 o && old 자식 x
+      if (newNodeObj[key].children.length && !oldNodeObj[key].children.length) {
+        target.querySelector(`[key="${key}"]`).replaceWith(newNodeObj[key]);
+
+        continue;
+      }
+
+      // new 자식 x && old 자식 o
+      if (!newNodeObj[key].children.length && oldNodeObj[key].children.length) {
+        const targetElement = target.querySelector(`[key="${key}"]`);
+
+        while (targetElement.firstChild) {
+          targetElement.removeChild(targetElement.firstChild);
+        }
+
+        continue;
+      }
+
+      // new 자식 o && old 자식 o
+      for (const childKey of Object.keys(newNodeObj[key])) {
+        rerender(target, getNodeObject(newNodeObj[key][childKey]), getNodeObject(oldNodeObj[key][childKey]));
+      }
     }
   };
 
-  return render;
+  const _render = (target: Element, node: Element): void => {
+    if (isEmptyObject(oldNodeObj)) {
+      target.appendChild(node);
+
+      getNodeObject(node, oldNodeObj);
+    } else {
+      const newNodeObj = getNodeObject(node);
+
+      rerender(target, newNodeObj, oldNodeObj);
+    }
+  };
+
+  return _render;
 })();
 
 export default render;
