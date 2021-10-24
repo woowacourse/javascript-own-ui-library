4640d1a3214540eebc17451c4db7f9e83eeae3ad
yujo <yujo.dev@gmail.com>
2021-10-25 01:23:50 +0900

    refactor: Component 렌더 시, key를 함께 렌더하도록 변경

--- src/components/Counter.ts
+++ src/components/Counter.ts
@@ -22,25 +22,25 @@ const reset = () => {
 const Counter = () => {
   return createElement(
     "div",
-    { class: "container" },
-    createElement("span", { class: "count" }, `${count}`),
+    { key: "0", class: "container" },
+    createElement("span", { key: "01", class: "count" }, `${count}`),
     createElement(
       "div",
-      { class: "btn-group" },
+      { key: "02", class: "btn-group" },
       createElement(
         "button",
-        { event: { click: decrease } },
-        createElement("strong", {}, "-")
+        { key: "020", event: { click: decrease } },
+        createElement("strong", { key: "0200" }, "-")
       ),
       createElement(
         "button",
-        { event: { click: reset } },
-        createElement("strong", {}, "RESET")
+        { key: "021", event: { click: reset } },
+        createElement("strong", { key: "0210" }, "RESET")
       ),
       createElement(
         "button",
-        { event: { click: increase } },
-        createElement("strong", {}, "+")
+        { key: "022", event: { click: increase } },
+        createElement("strong", { key: "0220" }, "+")
       )
     )
   );
