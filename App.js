1b6a224a6c7374255de5281b315e55910e8231ce
An Hyun Cheol <42052110+iborymagic@users.noreply.github.com>
2021-11-02 22:49:13 +0900

    [Virtual DOM 만들기] 피터(안현철) 미션 제출합니다. (#11)
    * feat: createElement 구현
    
    * feat: render 구현
    
    * feat: useState 구현
    
    * feat: render 함수 구조 변경
    - 시작 함수와 재귀 함수를 분리
    - documentFragment를 이용하도록 수정
    
    * feat: state와 useState 구현
    
    * refactor: Counter 컴포넌트를 App 컴포넌트에 삽입
    
    * fix: render 함수에서 Component 대신 Virtual DOM 객체를 인자로 받도록 수정
    
    * fix: diff 알고리즘 함수 제거
    
    * feat: diff 알고리즘(findDifference 함수) 구현
    
    * docs: readme.md 필수 요구사항 체크
    
    * fix: text node일 때 innerHTML 대신 textContent를 사용
    
    * refactor: 불필요한 optional chaining 삭제
    
    * refactor: 불필요한 변수 changed 삭제
    
    * fix: findDifference 함수에서 props 비교 시 optional chaining으로 방어
    
    * fix: 콜린 피드백 반영
    - realDOM의 children[0]을 firstChild로 수정
    - findDifference 함수명 updateDifference로 변경
    - children이 없는 경우 return해주는 조건문 위치 이동
    - forEach문이 아닌 for문에서 return하도록 수정
    - newChildren과 prevChildren이 children 갯수가 다른 경우를 위한 가드 추가

---
+++ App.js
@@ -0,0 +1,45 @@
+import { createElement } from './lib/Peact.js';
+import useState from './lib/useState.js';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return createElement(
+    'div',
+    { class: 'container' },
+    createElement('span', { class: 'count' }, createElement('text', {}, count)),
+    createElement(
+      'div',
+      { class: 'btn-group' },
+      createElement(
+        'button',
+        { click: decrease },
+        createElement('strong', {}, createElement('text', {}, '-'))
+      ),
+      createElement(
+        'button',
+        { click: reset },
+        createElement('strong', {}, createElement('text', {}, 'RESET'))
+      ),
+      createElement(
+        'button',
+        { click: increase },
+        createElement('strong', {}, createElement('text', {}, '+'))
+      )
+    )
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,4 @@
+import App from './App.js';
+import { render } from './lib/PeactDOM.js';
+
+render(App(), document.getElementById('root'));
