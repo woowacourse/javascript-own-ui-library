63f5e9ddea30a6d91f0cca58bdf7e568904b1da5
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-11-03 16:07:10 +0900

    refactor: createDOM에서 child가 text일 때의 로직 분리

--- src/lib/createVDOM.js
+++ src/lib/createVDOM.js
@@ -5,27 +5,30 @@ const createVDOM = (tag, attr, ...children) => {
     props: {},
   };
 
-  children.forEach((child) => {
-    if (child === undefined) return;
-
-    if (typeof child === "object") {
-      const currentChildren = element.props["children"];
-
-      if (Array.isArray(currentChildren)) {
-        element.props["children"].push(child);
-      } else {
-        element.props["children"] = [child];
-      }
-    } else {
-      element.props["children"] = child;
-    }
-  });
-
+  // 속성 부여
   if (attr) {
     for (const [key, value] of Object.entries(attr)) {
       element.props[key] = value;
     }
   }
+
+  children.forEach((child) => {
+    if (child === undefined) return;
+
+    if (typeof child === "string" || typeof child === "number") {
+      element.props = { ...element.props, text: child };
+    }
+
+    if (typeof child === "object") {
+      const currentChildren = element.props["children"];
+
+      if (Array.isArray(currentChildren)) {
+        element.props["children"] = [...currentChildren, child];
+      } else {
+        element.props["children"] = [child];
+      }
+    }
+  });
 
   return element;
 };
