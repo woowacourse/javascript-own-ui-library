a0a3ea864c60bd6b6e9eab91ddeed30ec98583b5
2sooy <1401660@naver.com>
2021-11-08 15:35:40 +0900

    feat: 비동기 처리 버튼 추가 및 상태 처리 시스템으로 redux 활용

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import Counter from './components/Counter.js';
 import { myReact, myReactDOM } from './lib/index.js';
 import { reducer, initialState } from './reducer.js';
 
+import { logger, thunk } from './lib/middleware.js';
+import createStore from './lib/store.js';
+
+const store = createStore(reducer, initialState, logger, thunk);
+
 const render = () => {
-  myReactDOM.render(myReact.createElement(Counter, null), document.querySelector('#root'));
+  myReactDOM.render(
+    myReact.createElement(Counter, { getState: store.getState, dispatch: store.dispatch }),
+    document.querySelector('#root')
+  );
 };
 
-export const myReactHook = myReact.createHook(render);
+store.subscribe(render);
 
 render(); // 최초 렌더링
