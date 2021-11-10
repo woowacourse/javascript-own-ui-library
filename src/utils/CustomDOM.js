4dee490612f21f98053f5bccddcdc0fa95d1e088
Chayan <hchayan196@gmail.com>
2021-11-10 20:30:11 +0900

    feat: 디렉터리 구조 개선

--- src/utils/CustomDOM.js
+++ src/utils/CustomDOM.js
@@ -2,25 +2,16 @@ import DOMStore from './DOMStore';
 
 const CustomDOM = {
   render: (element, rootElement) => {
-    // 1.VirtualDOM 만들기
     const newVirtualDOM = CustomDOM.getVDOM(element);
-
-    // 2. 기존 Virtual DOM과 비교하기
 
     if (!CustomDOM.isDifferent(rootElement, newVirtualDOM)) {
       return;
     }
 
-    // 3. 다르면 rerender
     rootElement.innerHTML = ``;
     CustomDOM.setVDOM(newVirtualDOM, rootElement);
   },
   getVDOM: element => {
-    // props는 현재 element에 속해있는 속성들을 가지고 있다.
-    // <div class='container' onClick={() => {}}> 의 경우
-    // ['class', 'container', 'onClick', f, children, Array(2)]
-    // 의 정보를 가지고 있다.
-    // 이 props를 순회해서, key값에 따라 처리해주면 된다.
     const { type, props } = element;
 
     // 1. Virtual DOM 만들기 (type, props, children 으로 구성)
