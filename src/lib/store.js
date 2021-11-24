8eb3a98a4a97f5e6313168201246936fbd869976
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-11-24 16:10:38 +0900

    refactor: Proxy가 render함수를 가지도록 코드 수정

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,11 +1,13 @@
-const store = (initialState, handler) => {
+import render from "./render.js";
+
+const store = (initialState, Element) => {
   return new Proxy(initialState, {
     get: function (target, key) {
       return target[key];
     },
     set: function (target, key, value) {
       target[key] = value;
-      handler();
+      render(Element());
       return true;
     },
   });
