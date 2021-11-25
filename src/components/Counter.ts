ad123af2f318f2c58b81dd4e9ed5295fa7d52cef
yujo <yujo.dev@gmail.com>
2021-11-26 01:25:32 +0900

    feat: Proxy 적용

--- src/components/Counter.ts
+++ src/components/Counter.ts
@@ -1,47 +1,37 @@
+import proxy from "../proxy/proxy";
 import createElement from "../utils/createElement";
-import render from "../utils/render";
 
-const $root = document.getElementById("root");
-
-let count = 0;
-
-const increase = () => {
-  ++count;
-  render($root, Counter());
-};
-
-const decrease = () => {
-  --count;
-  render($root, Counter());
-};
-const reset = () => {
-  count = 0;
-
-  console.log("in", count);
-  render($root, Counter());
+const countState = {
+  count: 0,
 };
 
 const Counter = () => {
+  const countProxy = proxy(countState, Counter);
+
+  const increaseCount = () => countProxy.count++;
+  const decreaseCount = () => countProxy.count--;
+  const resetCount = () => (countProxy.count = 0);
+
   return createElement(
     "div",
     { key: "0", class: "container" },
-    createElement("span", { key: "01", class: "count" }, `${count}`),
+    createElement("span", { key: "01", class: "count" }, `${countProxy.count}`),
     createElement(
       "div",
       { key: "02", class: "btn-group" },
       createElement(
         "button",
-        { key: "020", event: { click: decrease } },
+        { key: "020", event: { click: decreaseCount } },
         createElement("strong", { key: "0200" }, "-")
       ),
       createElement(
         "button",
-        { key: "021", event: { click: reset } },
+        { key: "021", event: { click: resetCount } },
         createElement("strong", { key: "0210" }, "RESET")
       ),
       createElement(
         "button",
-        { key: "022", event: { click: increase } },
+        { key: "022", event: { click: increaseCount } },
         createElement("strong", { key: "0220" }, "+")
       )
     )
